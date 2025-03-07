ee1e7d18346cf2659792ada9f83dbf8d7d3adc09
Alyaa289 <87453383+Alyaa289@users.noreply.github.com>
2025-03-07 16:22:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,6 @@ def spin_result():
     return jsonify({"message": "Result recorded"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000)
+    #app.run(debug=True)
+    
